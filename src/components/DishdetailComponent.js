71e57106bc3d673702e53381328fd5cb1892ff63
NIKHIL RANJAN <niks90975@gmail.com>
2020-07-26 18:42:53 +0530

    Single Page Application finish

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg,  CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg,  CardText, CardBody, CardTitle,Breadcrumb,BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         
@@ -36,15 +36,25 @@ import { Card, CardImg,  CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     const DishDetail = (props) => {
-        if(props.SelectedDish != null){
+        if(props.dish != null){
             return(
                 <div className="container">
                     <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                        <h3>{props.dish.name}}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
                         <div className="col-12 col-md-5 m-1">
-                            <RenderDish dish = {props.SelectedDish} />
+                            <RenderDish dish = {props.dish} />
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            <RenderComment comments = {props.SelectedDish.comments} /> 
+                            <RenderComment comments = {props.comments} /> 
                         </div>
                     </div>
                         
