b63074b7b9a9df31154e305eeb228fdad394f140
NIKHIL RANJAN <niks90975@gmail.com>
2020-07-27 20:18:19 +0530

    Updates

---
+++ precache-manifest.7be9f920ab659ff6eab54b68c5e0135f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba96eb185c6564c22ca7d45090f0ec0a",
+    "url": "/React-Confusion/index.html"
+  },
+  {
+    "revision": "d59d9018f45caf6b85ba",
+    "url": "/React-Confusion/static/css/2.d7fb18bc.chunk.css"
+  },
+  {
+    "revision": "7872e47a08a606833bf7",
+    "url": "/React-Confusion/static/css/main.45435c08.chunk.css"
+  },
+  {
+    "revision": "d59d9018f45caf6b85ba",
+    "url": "/React-Confusion/static/js/2.6da67622.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/React-Confusion/static/js/2.6da67622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7872e47a08a606833bf7",
+    "url": "/React-Confusion/static/js/main.69c93484.chunk.js"
+  },
+  {
+    "revision": "e2c56bdce0d59cc41c8b",
+    "url": "/React-Confusion/static/js/runtime-main.fc50f078.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
